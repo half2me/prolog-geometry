31f168ec4cdcac22d5c62f5057cd3a24e94d6fd3
Tamási Benjamin <halftome@yahoo.com>
2016-11-28 20:44:28 +0100

    Update

--- vector.pl
+++ vector.pl
@@ -1,11 +1,16 @@
-vec3(X-Y-Z) :-
-  number(X), number(Y), number(Z).
+vec3(_-_-_).
 
 add(AX-AY-AZ, BX-BY-BZ, VX-VY-VZ) :-
   VX is AX + BX, VY is AY + BY, VZ is AZ + BZ.
 
 sub(AX-AY-AZ, BX-BY-BZ, VX-VY-VZ) :-
   VX is AX - BX, VY is AY - BY, VZ is AZ - BZ.
+
+multiply(X-Y-Z, S, VX-VY-VZ) :-
+  VX is X*S, VY is Y*S, VZ is Z*S.
+
+divide(X-Y-Z, S, VX-VY-VZ) :-
+  VX is X/S, VY is Y/S, VZ is Z/S.
 
 dot(AX-AY-AZ, BX-BY-BZ, K) :-
   K is AX * BX + AY * BY + AZ * BZ.
@@ -14,3 +19,11 @@ cross(AX-AY-AZ, BX-BY-BZ, VX-VY-VZ) :-
   VX is AY * BZ - BY * AZ,
   VY is BX * AZ - AX * BZ,
   VZ is AX * BY - BX * AY.
+
+len(X-Y-Z, L) :-
+  L is sqrt(X**2 + Y**2 + Z**2).
+
+normal(V, N) :-
+  vec3(V), vec3(N),
+  len(V, L),
+  divide(V, L, N).
